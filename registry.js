c5155d0d340c28130156d8020340e307c2b983eb
Raif Harik <reharik@gmail.com>
2015-09-12 16:29:04 -0500

    commiting some shit for diagnosis

--- registry.js
+++ registry.js
@@ -6,9 +6,11 @@ var dagon = require('dagon');
 module.exports = function(_options) {
     var options = _options || {};
     var container = dagon(options.dagon);
-    return new container(x=>
+    var instance = new container(x=>
         x.pathToRoot(__dirname)
             .requireDirectoryRecursively('./src')
             .for('corelogger').renameTo('logger').instantiate(i=>i.asFunc().withParameters(options.logger || {}))
             .complete());
+    console.log(instance.whatDoIHave());
+    return instance;
 };
