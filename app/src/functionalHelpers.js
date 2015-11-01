d90c84c35523e0275a9fc6c65b1c35293090ad95
Raif Harik <reharik@gmail.com>
2015-11-01 11:48:59 -0600

    trying something new with matches function

--- app/src/functionalHelpers.js
+++ app/src/functionalHelpers.js
@@ -9,7 +9,7 @@ var log = function(x){ console.log(x); return x; };
 var safeProp = _.curry((x,o) => Maybe(o[x])); // !== 'undefined' ? Maybe(o[x]) : Maybe(null));
 var startsWith  = _.curry((x,s) => s.startsWith(x)? Maybe.of(true) : Maybe.Nothing());
 var doesNotStartWith = _.curry((x,s) => s.startsWith(x) ? Maybe.Nothing() : Maybe.of(true));
-var matches = _.curry((x,y)=> x===y ? Maybe(true) : Maybe.Nothing() );
+var matches = _.curry((x,y)=> x===y );
 var parseBuffer = function(item){ return buffer.Buffer.isBuffer(item) ? Maybe.of(JSON.parse(item.toString('utf8'))) : Maybe.Nothing() };
 
 return {
