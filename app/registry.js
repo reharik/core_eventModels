7b695a30640e2cc5b9da2c134f56a1e14aa0f454
Raif Harik <reharik@gmail.com>
2015-11-01 10:17:49 -0600

    fixing typo

--- app/registry.js
+++ app/registry.js
@@ -13,7 +13,7 @@ module.exports = function(_options) {
         x.pathToRoot(path.join(__dirname,'..'))
             .requireDirectoryRecursively('./app/src')
             .for('corelogger').renameTo('logger').instantiate(i=>i.asFunc().withParameters(options.logger || {}))
-    .for('functionalHelper').renameTo('fp')
+    .for('functionalHelper').renameTo('fh')
         .for('ramda').renameTo('_')
             .for('ramda-fantasy').renameTo('_fantasy')
             .complete());
