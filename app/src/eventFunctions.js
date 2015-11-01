9ac687e10ee5b1a898e937b94ea9d71368248c94
Raif Harik <reharik@gmail.com>
2015-11-01 10:50:05 -0600

    ug

--- app/src/eventFunctions.js
+++ app/src/eventFunctions.js
@@ -1,7 +1,7 @@
 
-module.exports = function(fh){
+module.exports = function(functionalHelpers){
 
-    //var fh = functionalHelper;
+    var fh = functionalHelper;
     var Maybe = _fantasy.Maybe;
     var getEventType = _.compose(_.chain(fh.safeProp('EventType')), fh.safeProp('Event'));
     var isNonSystemEvent = _.compose(_.chain(fh.doesNotStartWith('$')), getEventType);
