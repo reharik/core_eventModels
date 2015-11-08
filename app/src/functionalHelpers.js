9a92017f29e811e50440786eb9802a34c885061a
Raif Harik <reharik@gmail.com>
2015-11-08 09:55:55 -0600

    makeing matches a maybe. again

--- app/src/functionalHelpers.js
+++ app/src/functionalHelpers.js
@@ -9,7 +9,7 @@ var log = function(x){ console.log(x); return x; };
 var safeProp = _.curry((x,o) => Maybe(o[x])); // !== 'undefined' ? Maybe(o[x]) : Maybe(null));
 var startsWith  = _.curry((x,s) => s.startsWith(x));
 var doesNotStartWith = _.curry((x,s) =>  !s.startsWith(x) );
-var matches = _.curry((x,y)=> x===y );
+var mabyeMatches = _.curry((x,y)=> x===y ? Maybe.of(true) : Maybe.Nothing() );
 var parseBuffer = function(item){ return buffer.Buffer.isBuffer(item) ? Maybe.of(JSON.parse(item.toString('utf8'))) : Maybe.Nothing() };
 
 return {
