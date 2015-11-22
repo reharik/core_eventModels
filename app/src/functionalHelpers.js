bcce83501f6ab7e413a22a4d80994367abe4ea54
Raif Harik <reharik@gmail.com>
2015-11-21 22:08:03 -0600

    made safeprop more safe

--- app/src/functionalHelpers.js
+++ app/src/functionalHelpers.js
@@ -6,7 +6,7 @@ module.exports = function(_, _fantasy, buffer) {
 var Maybe = _fantasy.Maybe;
 
 var log = function(x){ console.log(x); return x; };
-var safeProp = _.curry((x,o) => Maybe(o[x]));
+var safeProp = _.curry((x,o) => o ? Maybe(o[x]) : Maybe.Nothing() );
 var startsWith  = _.curry((x,s) => s.startsWith(x) );
 var parseBuffer = x => buffer.Buffer.isBuffer(x) ? tryParseJSON(x.toString('utf8')) : Maybe.Nothing() ;
 var tryParseJSON = x => { try { return Maybe.of(JSON.parse(x)); }
